aba2236da5091d9c576e3c32369c89c4879deebf
Henry AT <yiperu@gmail.com>
2020-06-07 18:26:45 -0400

    First solution - We need inside this class, the names of users be active and order by ID

--- Fundamentos-FP-Playground.playground/Contents.swift
+++ Fundamentos-FP-Playground.playground/Contents.swift
@@ -19,8 +19,32 @@ class UserDatabase {
   
   // We need inside this class, the names of users be active and order by ID
 
+  var activeUserNames = [String]()
   
+  func storeActiveUserName() {
+    var activeUsers : [User] = []
+    for user in users {
+      if user.active == true {
+        activeUsers.append(user)
+      }
+    }
+    activeUsers.sort { (user1, user2) -> Bool in
+      user1.id < user2.id
+    }
+    
+    activeUserNames.removeAll()
+    
+    for user in activeUsers {
+      activeUserNames.append(user.name)
+    }
+  }
+    
 }
+
+let dataBase = UserDatabase()
+dataBase.storeActiveUserName()
+print(dataBase.activeUserNames)
+
 
 
 
