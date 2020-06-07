768f3949362f1c6bd9786b6d0bdab77644f25467
Henry AT <yiperu@gmail.com>
2020-06-07 18:14:22 -0400

    Make the scenary for make the change after

--- Fundamentos-FP-Playground.playground/Contents.swift
+++ Fundamentos-FP-Playground.playground/Contents.swift
@@ -1,5 +1,29 @@
 import UIKit
 
+struct User {
+  let id: Int
+  let name: String
+  let active: Bool
+}
+
+class UserDatabase {
+  let users = [
+    User(id: 1, name: "Bruce", active: false),
+    User(id: 2, name: "Juan", active: true),
+    User(id: 3, name: "Carlos", active: false),
+    User(id: 4, name: "Jhon", active: true),
+    User(id: 5, name: "Jose", active: false),
+    User(id: 6, name: "Tomas", active: true),
+    User(id: 7, name: "Gabriel", active: false)
+  ]
+  
+  // We need inside this class, the names of users be active and order by ID
+
+  
+}
+
+
+
 
 
 
