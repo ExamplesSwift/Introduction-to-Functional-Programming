bf4d5d07e7402a8ce9169f30a9919be94095806b
Henry AT <yiperu@gmail.com>
2020-06-07 20:10:09 -0400

    Try to make example to chec DNI , but it is cincomplete now

--- 05_Orientacion-a-exprexiones.playground/Contents.swift
+++ 05_Orientacion-a-exprexiones.playground/Contents.swift
@@ -1,3 +1,44 @@
 import UIKit
 
+
+import Foundation
+
 // Implement a program to check it DNI is correct
+
+let dni = "2345234"
+
+//func checkDni(dni: String) -> Bool {
+  let letters: [Character] = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "L"]
+  
+//var dniArray = Array(dni.character)
+
+
+var dniArray = Array(dni.character)
+
+
+
+//  if dniArray.count == 8 {
+//    dniArray.insert("0", at: 0)
+//  }
+//
+//  if dniArray.count != 9 {
+//    return false
+//  }
+//
+//  let dniNumber = String(dniArray[0...7])
+//
+//  guard let number = Int(dniNumber) else {
+//    return false
+//  }
+//
+//  let mod = number % 23
+//  let letter = letters[mod]
+//
+//  if dniArray[8] != letters {
+//    return false
+//  }
+//
+//  return true
+//}
+
+//checkDni(dni: "7124223L")
